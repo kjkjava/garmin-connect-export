74a663311c9fe8ef44407c68128c48b0203f39d5
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2019-05-06 20:18:30 -0700

    Progress: json dump of some activities looks sane.

--- gcdownload.py
+++ gcdownload.py
@@ -1,3 +1,5 @@
+import json
+
 from gclogin import GarminLogin
 from properties import Properties
 from deviceinfo import DeviceInfo
@@ -31,3 +33,25 @@ event_type_info = TypeInfo(session, event_type_url, event_properties)
 print("\nEvent Type Info:")
 event_type_info.do_print()
 
+# start of experimental get-activities code
+total_to_download = 5 # int(args.count)
+total_downloaded = 0
+url_gc_search = 'http://connect.garmin.com/modern/proxy/activitylist-service/activities/search/activities?'
+while total_downloaded < total_to_download:
+  # Maximum of 100... 400 return status if over 100.  So download 100 or whatever remains if less than 100.
+  if total_to_download - total_downloaded > 100:
+    num_to_download = 100
+  else:
+    num_to_download = total_to_download - total_downloaded
+
+  search_params = {'start': total_downloaded, 'limit': num_to_download}
+  http_data = session.get(url_gc_search, params=search_params)
+  json_results = json.loads(http_data.text)
+  # result = http_req(query_url)
+  # json_results = json.loads(result)  # TODO: Catch possible exceptions here.
+
+  print "### json_results:"
+  print json.dumps(json_results, indent=4, sort_keys=True)
+  print "###"
+  total_downloaded += num_to_download
+# end of experiment
